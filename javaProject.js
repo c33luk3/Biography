589ff4ba9c41f35d0b6badb6485b4f49d9040360
c33luk3 <ceeluke@gmail.com>
2019-01-30 16:00:22 -0500

    pushin it

---
+++ javaProject.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let topLeft,topRight,topCenter;
+let centerLeft,centerRight,centerCenter;
+let bottomLeft,bottomRight,botomCenter;
+
+
+let grid ="_|_|_\n_|_|_\n | |";
+console.log(grid);
+
+if (centerCenter != "x") {
+} else if(y == "x") {
+    console.log(centerCenter);
+}
+     
+
+if (centerLeft || centerRight == "x") {
+    y == "topCenter";
+    console.log(centerLeft);
+}
+if (topLeft || topCenter || topRight  == "x") {
+    y == "bottomLeft";
+}
+
+
+
--- readme.md
+++
@@ -1,4 +0,0 @@
-# :rocket: Welcome to my repository. :rocket:
-# :octocat: This is my second webpage I've built with HTML and CSS. :octocat:
-
-
